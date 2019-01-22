3c0957978b914f68b5ff186e749fff7cde146ccf
Fumi Kuranishi <kuranishi3221@gmail.com>
2019-01-22 11:34:46 +0100

    challenge4

--- src/index.js
+++ src/index.js
@@ -1,14 +1,31 @@
 class Character {
-  constructor() {
+  constructor(props) {
+    this.name = props.name;
+    this.hp = props.hp;
+    this.initialHp = props.initialHp;
+    this.mp = props.mp;
+    this.initialMp = props.initialMp;
+    this.offensePower = props.offensePower;
+    this.defencePower = props.defencePower;
   }
 
   showStatus() {
+    document.getElementById('main').innerHTML = `${this.name} HP : ${this.hp} / MP : ${this.mp}`;
     /* 
       キャラクターの名前、HP、MPを表示する。
     */
   }
 
   attack(defender) {
+    if (defender.hp !== 0) {
+      if (defender.hp <= this.offensePower) {
+        console.log(`${this.name}の攻撃！${defender.name}に???のダメージ！${defender.name}は力尽きた！`);
+      } else {
+        console.log(`${this.name}の攻撃！${defender.name}に???のダメージ！`);
+      }
+    } else {
+      console.log(`${this.name}の攻撃！しかし${defender.name}はHPが0なので攻撃できない！`)
+    }
     /*
       キャラクターが死んでいる場合は攻撃出来ないので、それを表示する。
       死んでいない場合は相手に与えたダメージを表示。
@@ -17,6 +34,12 @@ class Character {
   }
 
   calcAttackDamage(defender) {
+    const damage = this.offensePower - defender.defencePower;
+    if (damage < 0) {
+      return defender.hp - 1;
+    } else {
+      return defender.hp - damage;
+    }
     /*
       ダメージは単純に攻撃力から防御力を引いて計算する。
       ダメージが0未満の場合は、最低のダメージ1を与える。
@@ -24,49 +47,50 @@ class Character {
   }
 }
 
-class Sorcerer extends Character {
-  constructor() {
+
+// class Sorcerer extends Character {
+//   constructor() {
     
-  }
+//   }
 
-  healSpell(target) {
-    /* 
-      回復魔法は3のMPを消費する。
-      相手のHPを15回復する。
-      魔法使いが死んでいる場合はその旨を表示する。
-      相手が死んでいる場合は回復が出来ないためその旨を表示する。
-      MPが足りない場合はその旨を表示する。
-    */
-  }
+//   healSpell(target) {
+//     /* 
+//       回復魔法は3のMPを消費する。
+//       相手のHPを15回復する。
+//       魔法使いが死んでいる場合はその旨を表示する。
+//       相手が死んでいる場合は回復が出来ないためその旨を表示する。
+//       MPが足りない場合はその旨を表示する。
+//     */
+//   }
 
-  fireSpell(target) {
-    /* 
-      攻撃魔法は2のMPを消費する。
-      相手に10のダメージを与える。
-      魔法使いが死んでいる場合はその旨を表示する。
-      相手が死んでいる場合は攻撃が出来ないためその旨を表示する。
-      MPが足りない場合はその旨を表示する。
-    */
-  }
-}
+//   fireSpell(target) {
+//     /* 
+//       攻撃魔法は2のMPを消費する。
+//       相手に10のダメージを与える。
+//       魔法使いが死んでいる場合はその旨を表示する。
+//       相手が死んでいる場合は攻撃が出来ないためその旨を表示する。
+//       MPが足りない場合はその旨を表示する。
+//     */
+//   }
+// }
 
 {
   const fighter = new Character({
-    name: '武道家',
+    name: '武道家🕺',
     hp: 40,
     mp: 0,
     offensePower: 15,
     defencePower: 10
   })
-  const sorcerer = new Sorcerer({
-    name: '魔法使い',
-    hp: 25,
-    mp: 10,
-    offensePower: 8,
-    defencePower: 10
-  })
+  // const sorcerer = new Sorcerer({
+  //   name: '魔法使い👩‍🎨',
+  //   hp: 25,
+  //   mp: 10,
+  //   offensePower: 8,
+  //   defencePower: 10
+  // })
   const monster = new Character({
-    name: 'モンスター',
+    name: 'モンスター😈',
     hp: 60,
     mp: 0,
     offensePower: 30,
@@ -74,15 +98,15 @@ class Sorcerer extends Character {
   })
 
   fighter.attack(monster);
-  sorcerer.attack(monster);
-  monster.attack(sorcerer);
+  // sorcerer.attack(monster);
+  // monster.attack(sorcerer);
   fighter.attack(monster);
-  sorcerer.healSpell(sorcerer);
+  // sorcerer.healSpell(sorcerer);
   monster.attack(fighter);
   fighter.attack(monster);
-  sorcerer.fireSpell(monster);
+  // sorcerer.fireSpell(monster);
   monster.attack(fighter);
   fighter.showStatus();
-  sorcerer.showStatus();
+  // sorcerer.showStatus();
   monster.showStatus();
 }
